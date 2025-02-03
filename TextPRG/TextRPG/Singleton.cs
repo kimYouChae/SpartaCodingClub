dc11dcc84831ab39f1b094e21ef5a9a49f8ba3b6
김지현 <kjhstar2486@naver.com>
2025-02-03 14:29:44 +0900

    싱글톤 클래스 추가

---
+++ TextPRG/TextRPG/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    abstract class Singleton<T>
+        where T : class , new() // new 제약조건이 있어야 new T() 할 수 있음 
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get 
+            {
+                // null일때만 새로 생성
+                if (instance == null)
+                    instance = new T();
+
+                return instance;
+            }
+        }   
+    }
+}
