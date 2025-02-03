c4f7d763855391e2726e2e87fa918891300e2d6b
김지현 <kjhstar2486@naver.com>
2025-02-03 14:14:37 +0900

    Player, GameManager 클래스 추가

---
+++ TextPRG/TextRPG/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    class Player
+    {
+        private int _LV;
+        private PlayerClass _playerClass;
+        private string _name;
+        private float _attackPower;
+        private float _defencPower;
+        private float _HP;
+        private float _gold;
+
+        public Player(int lv, PlayerClass pClass, string name, float attak, float defence, float hp, float gold )
+        { 
+            this._LV = lv;
+            this._playerClass = pClass;
+            this._name = name;
+            this._attackPower = attak;
+            this._defencPower = defence;
+            this._HP = hp;
+            this._gold = gold;
+        }
+
+        public void Print() 
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("LV." + _LV.ToString() + '\n');
+            stringBuilder.Append("PlayerName" + _name + "(" + _playerClass.ToString() + " )" + '\n');
+            stringBuilder.Append("공격력 : " + _attackPower.ToString() + '\n');
+            stringBuilder.Append("방어력 : " + _attackPower.ToString() + '\n');
+            stringBuilder.Append("체력 : " + _HP.ToString() + '\n');
+            stringBuilder.Append("방어력 : " + _gold.ToString() + '\n');
+
+            Console.WriteLine(stringBuilder);
+
+            
+        }
+    }
+}
